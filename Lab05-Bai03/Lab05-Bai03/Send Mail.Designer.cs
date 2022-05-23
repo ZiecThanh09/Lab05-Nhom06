d90fb5ea3ac414b377bd7624df2b1c29c2c9d1a5
Thinh20521964 <20521964@gm.uit.edu.vn>
2022-05-23 18:44:36 +0700

    a

---
+++ Lab05-Bai03/Lab05-Bai03/Send Mail.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace Lab05_Bai03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btSend = new System.Windows.Forms.Button();
+            this.tbFrom = new System.Windows.Forms.TextBox();
+            this.tbTo = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbSubject = new System.Windows.Forms.TextBox();
+            this.rtbBody = new System.Windows.Forms.RichTextBox();
+            this.btClose = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.cbPassword = new System.Windows.Forms.CheckBox();
+            this.btAttachFile = new System.Windows.Forms.Button();
+            this.lbAttachFile = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(90, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(432, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(90, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Subject : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(90, 292);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Body : ";
+            // 
+            // btSend
+            // 
+            this.btSend.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSend.Location = new System.Drawing.Point(46, 50);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(121, 52);
+            this.btSend.TabIndex = 5;
+            this.btSend.Text = "SEND";
+            this.btSend.UseVisualStyleBackColor = false;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // tbFrom
+            // 
+            this.tbFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFrom.Location = new System.Drawing.Point(67, 36);
+            this.tbFrom.Name = "tbFrom";
+            this.tbFrom.Size = new System.Drawing.Size(319, 22);
+            this.tbFrom.TabIndex = 6;
+            // 
+            // tbTo
+            // 
+            this.tbTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTo.Location = new System.Drawing.Point(184, 199);
+            this.tbTo.Name = "tbTo";
+            this.tbTo.Size = new System.Drawing.Size(466, 22);
+            this.tbTo.TabIndex = 7;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(530, 34);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(319, 22);
+            this.tbPassword.TabIndex = 8;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // tbSubject
+            // 
+            this.tbSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSubject.Location = new System.Drawing.Point(184, 238);
+            this.tbSubject.Name = "tbSubject";
+            this.tbSubject.Size = new System.Drawing.Size(466, 22);
+            this.tbSubject.TabIndex = 9;
+            // 
+            // rtbBody
+            // 
+            this.rtbBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbBody.Location = new System.Drawing.Point(184, 292);
+            this.rtbBody.Name = "rtbBody";
+            this.rtbBody.Size = new System.Drawing.Size(466, 241);
+            this.rtbBody.TabIndex = 10;
+            this.rtbBody.Text = "";
+            // 
+            // btClose
+            // 
+            this.btClose.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btClose.Location = new System.Drawing.Point(529, 557);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(121, 52);
+            this.btClose.TabIndex = 11;
+            this.btClose.Text = "CLOSE";
+            this.btClose.UseVisualStyleBackColor = false;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // btClear
+            // 
+            this.btClear.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btClear.Location = new System.Drawing.Point(184, 557);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(121, 52);
+            this.btClear.TabIndex = 12;
+            this.btClear.Text = "CLEAR";
+            this.btClear.UseVisualStyleBackColor = false;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // cbPassword
+            // 
+            this.cbPassword.AutoSize = true;
+            this.cbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPassword.Location = new System.Drawing.Point(624, 63);
+            this.cbPassword.Name = "cbPassword";
+            this.cbPassword.Size = new System.Drawing.Size(145, 21);
+            this.cbPassword.TabIndex = 13;
+            this.cbPassword.Text = "Show password!";
+            this.cbPassword.UseVisualStyleBackColor = true;
+            this.cbPassword.CheckedChanged += new System.EventHandler(this.cbPassword_CheckedChanged);
+            // 
+            // btAttachFile
+            // 
+            this.btAttachFile.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btAttachFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAttachFile.Location = new System.Drawing.Point(878, 234);
+            this.btAttachFile.Name = "btAttachFile";
+            this.btAttachFile.Size = new System.Drawing.Size(167, 50);
+            this.btAttachFile.TabIndex = 14;
+            this.btAttachFile.Text = "Attach-File";
+            this.btAttachFile.UseVisualStyleBackColor = false;
+            this.btAttachFile.Click += new System.EventHandler(this.btAttachFile_Click);
+            // 
+            // lbAttachFile
+            // 
+            this.lbAttachFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAttachFile.FormattingEnabled = true;
+            this.lbAttachFile.HorizontalScrollbar = true;
+            this.lbAttachFile.ItemHeight = 16;
+            this.lbAttachFile.Location = new System.Drawing.Point(740, 292);
+            this.lbAttachFile.Name = "lbAttachFile";
+            this.lbAttachFile.ScrollAlwaysVisible = true;
+            this.lbAttachFile.Size = new System.Drawing.Size(421, 244);
+            this.lbAttachFile.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbFrom);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbPassword);
+            this.groupBox1.Controls.Add(this.cbPassword);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(184, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(887, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Your email";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1192, 627);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbAttachFile);
+            this.Controls.Add(this.btAttachFile);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.rtbBody);
+            this.Controls.Add(this.tbSubject);
+            this.Controls.Add(this.tbTo);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Send Mail";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.TextBox tbFrom;
+        private System.Windows.Forms.TextBox tbTo;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbSubject;
+        private System.Windows.Forms.RichTextBox rtbBody;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.CheckBox cbPassword;
+        private System.Windows.Forms.Button btAttachFile;
+        private System.Windows.Forms.ListBox lbAttachFile;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Lab05-Bai03/Lab05-Bai03/Send Mail.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+
+namespace Lab05_Bai03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra người dùng có nhập đủ thông tin hay chưa
+            if (tbFrom.Text == "" || tbPassword.Text == "" || tbTo.Text == "" || tbSubject.Text == "" || rtbBody.Text == "")
+            {
+                MessageBox.Show("There are some blanks. Please enter all the information!", "Notify", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                using (SmtpClient smtpClient = new SmtpClient("127.0.0.1"))
+                {
+                    string mailfrom = tbFrom.Text.ToString().Trim();
+                    string mailto = tbTo.Text.ToString().Trim();
+                    string password = tbPassword.Text.ToString().Trim();
+                    var basicCredential = new NetworkCredential(mailfrom, password);
+                    using (MailMessage message = new MailMessage())
+                    {
+                        MailAddress fromAddress = new MailAddress(mailfrom);
+                        smtpClient.UseDefaultCredentials = false;
+                        smtpClient.Credentials = basicCredential;
+                        message.From = fromAddress;
+                        message.Subject = tbSubject.Text.ToString().Trim();
+                        // Set IsBodyHtml to true means you can send HTML email.
+                        message.IsBodyHtml = true;
+                        message.Body = rtbBody.Text.ToString();
+                        message.To.Add(mailto);
+                        if (lbAttachFile.Items.Count > 0)
+                        {
+                            foreach (var filename in lbAttachFile.Items)
+                            {
+                                //Kiểm tra file có tồn tại trong ổ đĩa không
+                                if (File.Exists(filename.ToString()))
+                                {
+                                    //Thêm file đính kèm vào tin nhắn
+                                    message.Attachments.Add(new Attachment(filename.ToString()));
+                                }
+                            }
+                        }
+                        smtpClient.Send(message);
+                        MessageBox.Show("The mail is sent to " + tbTo.Text + " successfully!", "Notify", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There are some errors, so the mail can't be sent to " + tbTo.Text + "!", "Notify", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            tbFrom.Text = "";
+            tbTo.Text = "";
+            tbPassword.Text = "";
+            tbSubject.Text = "";
+            rtbBody.Text = "";
+            lbAttachFile.Items.Clear();
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cbPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if(cbPassword.Checked==true)
+            {
+                tbPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                tbPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void btAttachFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (var filename in ofd.FileNames)
+                {
+                    //Thêm các file đã chọn vào lbAttachFile
+                    lbAttachFile.Items.Add(filename.ToString());
+                }
+            }
+        }
+    }
+}
