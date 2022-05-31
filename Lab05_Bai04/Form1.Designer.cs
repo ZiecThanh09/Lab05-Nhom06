aaa0d4eb761a778b36e6b638a42a26867f5b3cd1
Anh Đức <104743721+ADUK2@users.noreply.github.com>
2022-05-31 11:52:49 +0700

    Add files via upload

---
+++ Lab05_Bai04/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Lab05_Bai04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbRecent = new System.Windows.Forms.TextBox();
+            this.tb_Total = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbRecent
+            // 
+            this.tbRecent.BackColor = System.Drawing.SystemColors.Control;
+            this.tbRecent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRecent.Location = new System.Drawing.Point(360, 133);
+            this.tbRecent.Name = "tbRecent";
+            this.tbRecent.Size = new System.Drawing.Size(32, 27);
+            this.tbRecent.TabIndex = 20;
+            this.tbRecent.Text = "0";
+            // 
+            // tb_Total
+            // 
+            this.tb_Total.BackColor = System.Drawing.SystemColors.Control;
+            this.tb_Total.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Total.Location = new System.Drawing.Point(93, 133);
+            this.tb_Total.Name = "tb_Total";
+            this.tb_Total.Size = new System.Drawing.Size(31, 27);
+            this.tb_Total.TabIndex = 19;
+            this.tb_Total.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(287, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Recent:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Total:";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(40, 187);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(724, 244);
+            this.listView1.TabIndex = 16;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.Location = new System.Drawing.Point(617, 48);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(147, 34);
+            this.btn_Login.TabIndex = 15;
+            this.btn_Login.Text = "LOGIN";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Email:";
+            // 
+            // tbPass
+            // 
+            this.tbPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPass.Location = new System.Drawing.Point(137, 82);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.PasswordChar = '*';
+            this.tbPass.Size = new System.Drawing.Size(291, 27);
+            this.tbPass.TabIndex = 12;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmail.Location = new System.Drawing.Point(137, 19);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(291, 27);
+            this.tbEmail.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbRecent);
+            this.Controls.Add(this.tb_Total);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbEmail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRecent;
+        private System.Windows.Forms.TextBox tb_Total;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.TextBox tbEmail;
+    }
+}
+
---
+++ Lab05_Bai04/Form1.cs
@@ -0,0 +1,63 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Lab05_Bai04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+            listView1.Columns.Add("Email", 200);
+            listView1.Columns.Add("From", 100);
+            listView1.Columns.Add("Thời gian", 100);
+            listView1.View = View.Details;
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var client = new ImapClient())
+                {
+                    client.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+                    string email = tbEmail.Text.ToString().Trim();
+                    string pass = tbPass.Text.ToString().Trim();
+                    client.Authenticate(email, pass);
+                    var inbox = client.Inbox;
+                    inbox.Open(FolderAccess.ReadOnly);
+                    tb_Total.Text = inbox.Count.ToString();
+                    tbRecent.Text = inbox.Count.ToString();
+                    for (int i = 0; i < inbox.Count; i++)
+                    {
+                        var message = inbox.GetMessage(i);
+                        ListViewItem name = new ListViewItem(message.Subject);
+                        ListViewItem.ListViewSubItem from = new
+                        ListViewItem.ListViewSubItem(name, message.From.ToString());
+                        name.SubItems.Add(from);
+                        ListViewItem.ListViewSubItem date = new
+                        ListViewItem.ListViewSubItem(name, message.Date.Date.ToString());
+                        name.SubItems.Add(date);
+                        listView1.Items.Add(name);
+                    }
+                    client.Disconnect(true);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Sai Email hoặc Password!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
